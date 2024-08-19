4580f2e36829dd4fa4b8d6ccd905729c6034d2f2
liupeng <laupeng1989@gmail.com>
2024-08-19 20:41:18 +0800

    minor fix.

--- rfwave/experiment_e2.py
+++ rfwave/experiment_e2.py
@@ -210,7 +210,7 @@ class VocosExp(pl.LightningModule):
         phone_info, pi_kwargs, ctx_kwargs = self.process_context(phone_info)
         with torch.no_grad():
             text = self.input_adaptor(*phone_info)
-            assert text.size(1) == pi_kwargs['num_tokens'].max() + pi_kwargs['ctx_length'].max(), \
+            assert text.size(2) == pi_kwargs['num_tokens'].max() + pi_kwargs['ctx_length'].max(), \
                 (f"text_ctx length {text.size(2)} != num_tokens {pi_kwargs['num_tokens'].max()} + "
                  f"ctx_length {pi_kwargs['ctx_length'].max()}")
             mel = self.feature_extractor(audio_input, **kwargs)
